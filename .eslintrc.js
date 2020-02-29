21648c7b39dff1db34c04d0f69895c4ea0e2acba
Julian Anning <julian.anning@gmail.com>
2020-02-29 10:45:28 +1000

    Basic linting rules and lint application

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,10 @@ module.exports = {
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'never']
   },
 };
